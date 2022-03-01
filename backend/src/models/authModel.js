69f7bafdea5735e9b9e1d1398fcbcd3d215f8e8c
AilinCarribero <42686226+AilinCarribero@users.noreply.github.com>
2022-02-28 21:18:10 -0300

    Back: - Posible solucion v6.2

--- backend/src/models/authModel.js
+++ backend/src/models/authModel.js
@@ -8,7 +8,6 @@ module.exports = ( sequelize, type ) => {
         nombre_apellido: type.STRING(60),
         correo: {
             type: type.STRING(100),
-            unique: true,
             validator: {
                 isEmail: true
             }
